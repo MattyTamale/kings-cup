0bbde8c87ab00ed8bb75eda6f60debcf377b4d0c
Matt Talamantes <matthew@Matts-MacBook-Pro.local>
2019-04-13 11:09:25 -0700

    Generated card images on index.html

--- game.js
+++ game.js
@@ -17,38 +17,43 @@ $(() => {
   //Drawing a single card at random from the deck of cards.
      $.ajax({
               type: "GET",
-              url: "https://deckofcardsapi.com/api/deck/new/draw/?count=1",
+              url: "https://deckofcardsapi.com/api/deck/new/draw/?count=52",
               dataType: "json",
               success: (data)=>{
                  console.log(data);
+//Creates a for loop that appends every card image to the body, for now
+                 for (let i = 0; i < data.cards.length; i++){
+                   const $newCard = $('<img>').attr('src', data.cards[i].image);
+                   $('body').append($newCard);
+               }
               },
               error: ()=>{
                       console.log('bad request');
           }
         })
     //Created a Discard Pile:
-      $.ajax({
-              type: "GET",
-               url: "https://deckofcardsapi.com/api/deck/new/pile/discard_pile/add/?cards=AS,2S",
-               dataType: "json",
-               success: (data)=>{
-                 console.log(data);
-              },
-              error: ()=>{
-                      console.log('bad request');
-          }
-        })
+      // $.ajax({
+      //         type: "GET",
+      //          url: "https://deckofcardsapi.com/api/deck/new/pile/discard_pile/add/?cards=AS,2S",
+      //          dataType: "json",
+      //          success: (data)=>{
+      //            console.log(data);
+      //         },
+      //         error: ()=>{
+      //                 console.log('bad request');
+      //     }
+      //   })
     //Created a listing of discarded cards for checking win/lose condition.
-        $.ajax({
-                type: "GET",
-                 url: "https://deckofcardsapi.com/api/deck/new/pile/discard_pile/list",
-                 dataType: "json",
-                 success: (data)=>{
-                   console.log(data);
-                },
-                error: ()=>{
-                        console.log('bad request');
-            }
-          })
+        // $.ajax({
+        //         type: "GET",
+        //          url: "https://deckofcardsapi.com/api/deck/new/pile/discard_pile/list",
+        //          dataType: "json",
+        //          success: (data)=>{
+        //            console.log(data);
+        //         },
+        //         error: ()=>{
+        //                 console.log('bad request');
+        //     }
+        //   })
 
 });
